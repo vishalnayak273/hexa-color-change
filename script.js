2d952da31c2b6b6df0422094f0e3ee8f084c9c71
vishalnayak273 <157096394+vishalnayak273@users.noreply.github.com>
2024-05-30 12:03:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let get=()=>{
+    let randomnumber=Math.floor(Math.random()*16777215);
+    let randomcode = "#"+randomnumber.toString(16)
+    document.body.style.backgroundColor=randomcode
+    document.getElementById("code").innerHTML=randomcode
+}
+
+
+document.getElementById("btn").addEventListener("click",get)
+
+get();
